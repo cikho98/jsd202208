cd3f228dbaa202429f2b1de686ff63c03b23c3e8
cikho98 <1165667817@qq.com>
2022-09-07 17:06:37 +0800

    study

--- src/day06/MethodDemo.java
+++ src/day06/MethodDemo.java
@@ -8,9 +8,9 @@ public class MethodDemo {
         say();
 
         sayHi("cikho");
-        sayHi("cikho",25);
+        sayHi("cikho", 25);
 
-        double a= getNum();
+        double a = getNum();
         System.out.println(a);
 
         Random ran = new Random();
@@ -19,54 +19,49 @@ public class MethodDemo {
         int num2 = ran.nextInt(100);
 
 
-        int num = plus(num1,num2);
+        int num = plus(num1, num2);
         System.out.println(num);
 
 
-        int[] arr= createArrays(10,100);
+        int[] arr = createArrays(10, 100);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
-        };
-
-
-
-
+        }
+        ;
 
 
     }
 
-    public static  int[] createArrays(int num,int ran_num){
-        int[] arrays= new int[num];
+    public static int[] createArrays(int num, int ran_num) {
+        int[] arrays = new int[num];
         Random ran = new Random();
         for (int i = 0; i < num; i++) {
-            arrays[i]=ran.nextInt(ran_num);
+            arrays[i] = ran.nextInt(ran_num);
         }
         return arrays;
     }
 
 
-
-    public static void say(){
+    public static void say() {
         System.out.println("halo");
     }
 
-    public static void sayHi(String name){
-        System.out.println("大家好，我叫"+name+",今年24岁了");
+    public static void sayHi(String name) {
+        System.out.println("大家好，我叫" + name + ",今年24岁了");
     }
 
-    public static void sayHi(String name,int age){
-        System.out.println("大家好，我叫"+name+",今年"+age+"岁了");
+    public static void sayHi(String name, int age) {
+        System.out.println("大家好，我叫" + name + ",今年" + age + "岁了");
     }
 
-    public static double getNum(){
+    public static double getNum() {
         return 8.88;
     }
 
-    public static int plus(int num1,int num2){
-        int num = num1=num2;
-        return  num;
+    public static int plus(int num1, int num2) {
+        int num = num1 = num2;
+        return num;
     }
-
 
 
 }
